58d71b57255b98f3f998bd8eb1bab6926df9c219
aigerimkwon <kwonmailbox@gmail.com>
2022-11-01 15:26:45 -0400

    added props, state, handlers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState} from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
+
+  const [ newTodo, setNewTodo]=useState('');
 
   return (
     <div className="App">
       <h1> Todo List</h1>
       <div>
         <TodoList />
-        <AddTodoForm />
+        <AddTodoForm 
+          onAddTodo ={setNewTodo}
+        />
+        <p>
+          {newTodo}
+        </p>
       </div>
-   </div>
+    </div>
   );
 }
 
