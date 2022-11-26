24c119b2f11fa1d68f755ddd29c0789944ad8b76
aigerimkwon <kwonmailbox@gmail.com>
2022-11-25 22:10:13 -0500

    added fragment, use effect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React, { useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
+import { Fragment } from 'react';
 
+
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = useState(() => {
+    const savedTodoList = JSON.parse(localStorage.getItem("savedTodoList"));
+    return savedTodoList || [];
+  }); 
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList))
+
+  }, [todoList]);
+  return [todoList, setTodoList];
+};
 function App() {
-
-  const [ todoList, setTodoList ] = useState([]); 
+  const [todoList, setTodoList] = useSemiPersistentState();
 
   const addTodo = (newTodo) => {
     setTodoList ([...todoList, newTodo]);
-  }
+  };
 
 
   return (
-    <div className="App">
+    <>
       <h1> Todo List</h1>
-      <div>
         <AddTodoForm  onAddTodo ={addTodo}/>
         <TodoList todoList ={todoList} />
-      </div>
-    </div>
+    </>
   );
 }
 
