c9416ec439c2fb8dacc25a0f271009abf941a1f8
aigerimkwon <kwonmailbox@gmail.com>
2023-03-06 13:56:57 -0500

    final changes

--- src/App.js
+++ src/App.js
@@ -21,48 +21,50 @@ function App() {
   return (
     <ThemeContext.Provider value={{ mode, toggleMode }}>
       <BrowserRouter>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <div className="App" id={mode}>
-                <div className="header">
-                  <Navbar />
-                  <div className="switch">
-                    <ReactSwitch
-                      onChange={toggleMode}
-                      checked={mode === "dark"}
-                      onColor="#fff"
-                      offColor="#0b1e38"
-                      onHandleColor="#0b1e38"
-                      offHandleColor="#fff"
-                      handleDiameter={25}
-                      height={30}
-                      width={68}
-                      uncheckedIcon={<UncheckedIcon />}
-                      checkedIcon={<ChekedIcon />}
-                    />
-                  </div>
-                </div>
+        <div className="App" id={mode}>
+          <div className="header">
+            <Navbar />
+            <div className="switch">
+              <ReactSwitch
+                onChange={toggleMode}
+                checked={mode === "dark"}
+                onColor="#fff"
+                offColor="#0b1e38"
+                onHandleColor="#0b1e38"
+                offHandleColor="#fff"
+                handleDiameter={25}
+                height={30}
+                width={68}
+                uncheckedIcon={<UncheckedIcon />}
+                checkedIcon={<ChekedIcon />}
+              />
+            </div>
+          </div>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
                 <TodoContainer
                   todoList={todoList}
                   setTodoList={setTodoList}
                   isLoading={isLoading}
                   setIsLoading={setIsLoading}
                 />
-              </div>
-            }
-          />
-          <Route
-            path="/new"
-            element={
-              <>
-                <h1 style={{ textAlign: "center" }}>New Todo List</h1>
-              </>
-            }
-          />
-        </Routes>
+              }
+            />
+            <Route
+              path="/new"
+              element={
+                <>
+                  <h1 style={{ textAlign: "center", color: "#009db7" }}>
+                    New Todo List
+                  </h1>
+                </>
+              }
+            />
+          </Routes>
+        </div>
       </BrowserRouter>
     </ThemeContext.Provider>
   );
